c5eabf504fc977f4935ff45aa23c8ee74e8f60b1
Gwen Stevens <gweneveirstevens@gmail.com>
2023-12-06 20:01:33 -0500

    GS: parses Game from string input

--- src/main/java/GameInspector.java
+++ src/main/java/GameInspector.java
@@ -1,6 +1,7 @@
 import model.Game;
 import model.Round;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,12 +12,42 @@ public class GameInspector {
 
     public Game parseGame(String input) {
         Game game = new Game();
-        game.setId(getId(input));
+        game.setId(getNumber(input));
+
+        String[] rounds = input
+                .substring(input.indexOf(":")+2)
+                .split(";");
+
+        game.setRounds(
+                Arrays.stream(rounds)
+                        .map(this::parseRound)
+                        .toList()
+        );
 
         return game;
     }
 
-    private int getId(String input) {
+
+    private Round parseRound(String round){
+        Round parsedRound = new Round();
+        parsedRound.setBlue(getAmountForColor(round, "blue"));
+        parsedRound.setRed(getAmountForColor(round, "red"));
+        parsedRound.setGreen(getAmountForColor(round, "green"));
+        return parsedRound;
+    }
+
+    private int getAmountForColor(String input, String color){
+        Matcher colorMatcher = Pattern
+                .compile("\\d+ "+color)
+                .matcher(input);
+
+        if (colorMatcher.find()){
+            return getNumber(colorMatcher.group());
+        } else return 0;
+
+    }
+
+    private int getNumber(String input) {
         Matcher matcher = Pattern
                 .compile("\\d+")
                 .matcher(input);
