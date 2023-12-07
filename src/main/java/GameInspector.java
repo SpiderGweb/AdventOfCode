e5dfc77fa29d9f4369190f558b5293582702b418
Gwen Stevens <gweneveirstevens@gmail.com>
2023-12-06 20:05:59 -0500

    GS - moves parsing game from the inspector to the game class constructor

--- src/main/java/GameInspector.java
+++ src/main/java/GameInspector.java
@@ -1,58 +1,8 @@
-import model.Game;
 import model.Round;
-
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class GameInspector {
     public boolean isRoundValid(Round exampleBag) {
         return false;
     }
 
-    public Game parseGame(String input) {
-        Game game = new Game();
-        game.setId(getNumber(input));
-
-        String[] rounds = input
-                .substring(input.indexOf(":")+2)
-                .split(";");
-
-        game.setRounds(
-                Arrays.stream(rounds)
-                        .map(this::parseRound)
-                        .toList()
-        );
-
-        return game;
-    }
-
-
-    private Round parseRound(String round){
-        Round parsedRound = new Round();
-        parsedRound.setBlue(getAmountForColor(round, "blue"));
-        parsedRound.setRed(getAmountForColor(round, "red"));
-        parsedRound.setGreen(getAmountForColor(round, "green"));
-        return parsedRound;
-    }
-
-    private int getAmountForColor(String input, String color){
-        Matcher colorMatcher = Pattern
-                .compile("\\d+ "+color)
-                .matcher(input);
-
-        if (colorMatcher.find()){
-            return getNumber(colorMatcher.group());
-        } else return 0;
-
-    }
-
-    private int getNumber(String input) {
-        Matcher matcher = Pattern
-                .compile("\\d+")
-                .matcher(input);
-
-        matcher.find();
-        return Integer.parseInt(matcher.group());
-    }
 }
---
+++ src/test/java/GameTest.java
@@ -0,0 +1,84 @@
+import model.Game;
+import model.Round;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class GameTest {
+    GameInspector gameInspector;
+
+    @BeforeEach
+    public void init() {
+        gameInspector = new GameInspector();
+    }
+
+    @Test
+    public void isRoundValidDefaultsToFalse() {
+        Round inputBag = new Round();
+
+        assertFalse(gameInspector.isRoundValid(inputBag));
+    }
+
+    @Test
+    public void parseGameSetsGameIdWhenItIsSingleDigit() {
+        String input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        Game game = new Game(input);
+
+        assertEquals(1, game.getId());
+    }
+
+    @Test
+    public void parseGameSetsGameIdWhenItIsMultiDigit() {
+        String input = "Game 356: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        Game game = new Game(input);
+
+        assertEquals(356, game.getId());
+    }
+
+
+    @Test
+    public void parseGameSetsParsesRoundWhenThereIsASingleRoundOfASingleColor() {
+        String input = "Game 356: 3 blue";
+        Game game = new Game(input);
+
+        assertEquals(1, game.getRounds().size());
+        assertEquals(3, game.getRounds().get(0).getBlue());
+        assertEquals(0, game.getRounds().get(0).getRed());
+        assertEquals(0, game.getRounds().get(0).getGreen());
+    }
+
+    @Test
+    public void parseGameSetsParsesRoundWhenThereIsASingleRoundOfManyColors() {
+        String input = "Game 356: 6 blue, 4 red";
+        Game game = new Game(input);
+
+        assertEquals(1, game.getRounds().size());
+        assertEquals(6, game.getRounds().get(0).getBlue());
+        assertEquals(4, game.getRounds().get(0).getRed());
+        assertEquals(0, game.getRounds().get(0).getGreen());
+    }
+
+    @Test
+    public void parseGameSetsParsesRoundWhenThereAreMultipleRoundsOfMultipleColors() {
+        String input = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
+        Game game = new Game(input);
+
+        assertEquals(4, game.getId());
+        assertEquals(3, game.getRounds().size());
+
+        assertEquals(6, game.getRounds().get(0).getBlue());
+        assertEquals(3, game.getRounds().get(0).getRed());
+        assertEquals(1, game.getRounds().get(0).getGreen());
+
+        assertEquals(0, game.getRounds().get(1).getBlue());
+        assertEquals(6, game.getRounds().get(1).getRed());
+        assertEquals(3, game.getRounds().get(1).getGreen());;
+
+        assertEquals(15, game.getRounds().get(2).getBlue());
+        assertEquals(14, game.getRounds().get(2).getRed());
+        assertEquals(3, game.getRounds().get(2).getGreen());
+    }
+
+}
